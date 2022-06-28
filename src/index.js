a7611798fda09382bb8e069201512eab8d780343
OksanaKuzich <komoks255@gmail.com>
2022-06-28 16:15:19 +0300

    fixed js errors

--- src/index.js
+++ src/index.js
@@ -2,13 +2,8 @@ import './js/modal';
 import './js/mobile-menu';
 import './js/map-modal';
 import './js/fslightbox';
-
-
-
-
-
-
 import './js/modal-mobile';
 import './js/franchise-modal';
 import './js/about-modal';
 import './js/read-more';
+import './js/slider';
--- src/js/modal-mobile.js
+++ src/js/modal-mobile.js
@@ -1,15 +1,10 @@
-(() => {
-    const refs = {
-      openModalBtn: document.querySelector('[data-modal-open-mobile]'),
-      closeModalBtn: document.querySelector('[data-modal-close-mobile]'),
-      modal: document.querySelector('[data-modal]'),
-    };
-  
-    refs.openModalBtn.addEventListener('click', toggleModal);
-    refs.closeModalBtn.addEventListener('click', toggleModal);
-  
-    function toggleModal() {
-      refs.modal.classList.toggle('is-hidden');
-    }
-  })();
-  
+const refs = {
+  openModalBtn: document.querySelector('[data-modal-open-mobile]'),
+  modal: document.querySelector('[data-modal]'),
+};
+
+refs.openModalBtn.addEventListener('click', toggleModal);
+
+function toggleModal() {
+  refs.modal.classList.toggle('is-hidden');
+}
